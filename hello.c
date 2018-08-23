672c5e001844194e9ddd7c2520965e272fe577c6
ChrisWhite3 <ChrisWhite3@users.noreply.github.com>
2018-08-23 16:27:03 -0500

    Initial Commit

---
+++ hello.c
@@ -0,0 +1,13 @@
+/**
+ * Christapher White
+ * 08/23/18
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Christapher White, Computer Engineering\n");
+
+  return 0;
+}
